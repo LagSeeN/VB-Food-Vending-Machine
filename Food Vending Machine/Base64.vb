abd8b8071c57f0c8c957f5b38c5cce2e7d7e448b
LagSeeN <19259262+LagSeeN@users.noreply.github.com>
2021-02-16 21:56:38 +0700

    :package: add base64 class

---
+++ Food Vending Machine/Base64.vb
@@ -0,0 +1,12 @@
+﻿Imports System.IO
+Public Class Base64
+    Public Function ConvertBase64ToByteArray(base64 As String) As Byte()
+        Return Convert.FromBase64String(base64) 'Convert the base64 back to byte array.
+    End Function
+
+    Public Function convertbytetoimage(ByVal BA As Byte())
+        Dim ms As MemoryStream = New MemoryStream(BA)
+        Dim image = System.Drawing.Image.FromStream(ms)
+        Return image
+    End Function
+End Class
