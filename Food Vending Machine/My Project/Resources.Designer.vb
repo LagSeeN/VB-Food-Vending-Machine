11d9e02b51db9493771f8ca21b920bc67ee79f5c
LagSeeN <19259262+LagSeeN@users.noreply.github.com>
2021-02-26 12:18:38 +0700

    :bug: Fix a bug

--- Food Vending Machine/My Project/Resources.Designer.vb
+++ Food Vending Machine/My Project/Resources.Designer.vb
@@ -151,6 +151,16 @@ Namespace My.Resources
         End Property
         
         '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        '''</summary>
+        Friend ReadOnly Property icon() As System.Drawing.Icon
+            Get
+                Dim obj As Object = ResourceManager.GetObject("icon", resourceCulture)
+                Return CType(obj,System.Drawing.Icon)
+            End Get
+        End Property
+        
+        '''<summary>
         '''  Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         '''</summary>
         Friend ReadOnly Property Sound() As System.IO.UnmanagedMemoryStream
